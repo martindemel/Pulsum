d6ddbd656bd010e2b175257d789989362172d611
Martin Demel <martin.demel@gmail.com>
2026-02-06 22:05:43 -0600

    Fix AppViewModel deinit for Swift 6

--- Packages/PulsumUI/Sources/PulsumUI/PulseView.swift
+++ Packages/PulsumUI/Sources/PulsumUI/PulseView.swift
@@ -417,14 +417,16 @@ private enum PulseHapticStyle { case medium, heavy }
 
 #if canImport(UIKit)
 private func performPulseHaptic(_ style: PulseHapticStyle) {
-    let mapped: UIImpactFeedbackGenerator.FeedbackStyle = {
-        switch style {
-        case .medium: return .medium
-        case .heavy: return .heavy
-        }
-    }()
-    let generator = UIImpactFeedbackGenerator(style: mapped)
-    generator.impactOccurred()
+    Task { @MainActor in
+        let mapped: UIImpactFeedbackGenerator.FeedbackStyle = {
+            switch style {
+            case .medium: return .medium
+            case .heavy: return .heavy
+            }
+        }()
+        let generator = UIImpactFeedbackGenerator(style: mapped)
+        generator.impactOccurred()
+    }
 }
 #else
 private func performPulseHaptic(_ style: PulseHapticStyle) {}
